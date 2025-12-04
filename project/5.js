ff2b2a28eb216364eaf4b2e02dfbbb340b7dc1d2
Anita Redno Sari <anitarednosari001@gmail.com>
2025-12-05 01:33:19 +0700

    5.js

---
+++ project/5.js
@@ -0,0 +1,4 @@
+document.getElementById('amplop').addEventListener('click', function() {
+    // Menambahkan atau menghapus kelas CSS 'terbuka' saat diklik
+    this.classList.toggle('terbuka');
+});
