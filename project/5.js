1de0c1dfb112fcdf9a8842d08f520d3182e1556c
Anita Redno Sari <anitarednosari001@gmail.com>
2025-12-05 03:17:20 +0700

    5.js

--- project/5.js
+++ project/5.js
@@ -1,5 +1,26 @@
-let letter = document.getElementById("letter");
+const letter = document.getElementById("letter");
+const popup = document.getElementById("popup");
+const closeBtn = document.getElementById("closeBtn");
 
+// Klik kertas → naik + buka popup
 letter.addEventListener("click", () => {
-    letter.classList.toggle("open");
+    letter.classList.add("open");
+
+    setTimeout(() => {
+        popup.style.display = "flex";
+    }, 450);
+});
+
+// Tutup popup
+closeBtn.addEventListener("click", () => {
+    popup.style.display = "none";
+    letter.classList.remove("open");
+});
+
+// Klik area luar popup → tutup
+window.addEventListener("click", (e) => {
+    if (e.target === popup) {
+        popup.style.display = "none";
+        letter.classList.remove("open");
+    }
 });
