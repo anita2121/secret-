bf0a9d6d66741a29bb27250da7f82e4c01836cd1
Anita Redno Sari <anitarednosari001@gmail.com>
2025-12-05 05:53:50 +0700

    5.js

--- project/5.js
+++ project/5.js
@@ -1,23 +1,26 @@
 let step = 0;
+let paperShown = false; // WAJIB ADA
 
 const paper = document.getElementById("paper");
 const popup = document.getElementById("popup");
 const closeBtn = document.getElementById("closeBtn");
+const bgm = document.getElementById("bgm"); // AMBIL AUDIO
 
 document.addEventListener("click", () => {
     step++;
 
-        // Klik pertama
+    // Klik pertama: tarik kertas + mulai musik
     if (!paperShown) {
         paper.classList.add("show");
         paperShown = true;
 
         // mulai musik setelah klik pertama
-        try { bgm.play(); } catch(e) {}
+        try { 
+            bgm.play(); 
+        } catch (e) {}
 
         return;
     }
-
 
     // Klik kedua → popup muncul
     if (step === 2) {
@@ -26,8 +29,8 @@ document.addEventListener("click", () => {
     }
 });
 
-// Tombol close
+// Tombol close popup
 closeBtn.addEventListener("click", (e) => {
     popup.style.display = "none";
-    e.stopPropagation(); // Biar tidak memicu klik global
+    e.stopPropagation();
 });
